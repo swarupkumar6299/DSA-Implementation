a1743a9dbf89d5a5eee90eba3f12ace1adb13fad
Swarup Kumar <87602927+swarupkumar6299@users.noreply.github.com>
2024-09-10 12:22:51 +0530

    Time: 4278 ms (5%), Space: 0B (100%) - LeetHub

---
+++ 1942-primary-department-for-each-employee/1942-primary-department-for-each-employee.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+SELECT employee_id, CASE WHEN COUNT(department_id) = 1
+THEN department_id
+WHEN COUNT(department_id)>1 THEN SUM((primary_flag='y')*department_id)END AS department_id
+FROM Employee
+GROUP BY employee_id
