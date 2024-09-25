3c0d63813d9309e5604cfa25495f6686fdc4818e
Swarup Kumar <87602927+swarupkumar6299@users.noreply.github.com>
2024-09-25 11:03:15 +0530

    Time: 3011 ms (30.35%), Space: 384.2 MB (32.07%) - LeetHub

---
+++ 2494-sum-of-prefix-scores-of-strings/2494-sum-of-prefix-scores-of-strings.py
@@ -0,0 +1,32 @@
+class PrefixNode:
+    def __init__ (self):
+        self.children = [None]*26
+        self.count = 0
+class PrefixTree:
+    def __init__(self):
+        self.root = PrefixNode()
+    def insert(self,w):
+        cur = self.root
+        for c in w:
+            i = ord(c) - ord('a')
+            if not cur.children[i]:
+                cur.children[i] = PrefixNode()
+            cur = cur.children[i]
+            cur.count += 1
+    def get_score(self,w):
+        cur= self.root
+        score = 0
+        for c in w:
+            i = ord(c) - ord('a')
+            cur = cur.children[i]
+            score += cur.count
+        return score
+class Solution:
+    def sumPrefixScores(self, words: List[str]) -> List[int]:
+        res = []
+        Prefix_tree = PrefixTree()
+        for w in words:
+            Prefix_tree.insert(w)
+        for w in words:
+            res.append(Prefix_tree.get_score(w))
+        return res        
